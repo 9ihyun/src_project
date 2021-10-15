85c0d705059fd4fa44adc2adf1a18eb1da62dce8
hyunco <82146251+hyunco@users.noreply.github.com>
2021-10-15 15:10:24 +0900

    update

---
+++ src/main/java/kosta/mvc/servlet/UserFindServlet.java
@@ -0,0 +1,55 @@
+package kosta.mvc.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kosta.mvc.controller.UserController;
+import kosta.mvc.dao.UserDAO;
+import kosta.mvc.dao.UserDAOImpl;
+import kosta.mvc.dto.User;
+
+/**
+ * 아이디, 비번을 찾아주는 서블릿
+ */
+@WebServlet("/userFindServlet")
+public class UserFindServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 //아이디 찾기
+		try {
+		         //입력 : birth, question, anwser  -----> UserDto
+		         request.setCharacterEncoding("UTF-8");
+		         User user = new User(); 
+		         user.setBirth(Integer.parseInt(request.getParameter("birth")));
+		         user.setPwq(request.getParameter("question"));
+		         user.setPwa(request.getParameter("anwser"));
+		         
+		         //처리
+		         UserDAO dao = new UserDAOImpl();
+		         String UserId =dao.myInfo(user);		        
+		         
+		         //출력
+		         if(UserId != null) {//결과가 있으면(정보가 맞다면)
+		            response.sendRedirect("find_id_result.jsp?UserId="+UserId);
+		            
+		            request.getSession().setAttribute("UserId", UserId);
+		            response.sendRedirect("find_id_result.jsp");
+		         }
+		         else {//결과가 없으면(정보가 맞지 않으면)
+		            response.sendRedirect("find_id.jsp?error");
+		         }
+		      }
+		      catch(Exception e) {
+		         e.printStackTrace();
+		         response.sendError(500);
+		      }
+		   }
+	}
+
+
--- src/main/webapp/user/searchPassword.jsp
+++ src/main/webapp/user/searchPassword.jsp
@@ -14,10 +14,10 @@
 </head>
 <body>
 	<p>아이디/비밀번호 찾기</p>
-   <form action="findUser.jsp" method="post">
+   <form action="userFindServlet" method="post">
     생년월일: <input type="text" name="birth" placeholder="8자리로 입력하세요." required><br>
     비밀번호 찾기 질문 : 
-	<select name="question">
+	<select name="question" required>
        <option value="0">--질문 선택--</option>
 	   <option value="one">보물 1호는?</option>
 	   <option value="two">어렸을 적 별명은?</option>
@@ -25,9 +25,14 @@
 	   <option value="four">가장 자주가는 음식점은?</option>
 	   <option value="five">만약 물고기를 한마리 기른다면?</option>	
 	</select> <br>
-    비밀번호 찾기 답: <input type="text" name="anwser"><br>
+    비밀번호 찾기 답: <input type="text" name="anwser" required><br>
     <input type="submit" value="확인"><br>
 </form>
+
+<!-- error에 대한 처리 -->
+   <%if(request.getParameter("error") != null){ %>
+   <h6><font color="red">해당하는 정보로 아이디를 찾지 못했습니다</font></h6>
+   <%} %>
  
 </body>
 </html>
