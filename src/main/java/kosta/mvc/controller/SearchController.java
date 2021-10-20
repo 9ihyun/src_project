0775605cc0dd7679cb6ee322d08aa12ea1a0eb89
JSH1995 <44968599+JSH1995@users.noreply.github.com>
2021-10-21 01:22:06 +0900

    스터디 검색 테스트

--- src/main/java/kosta/mvc/controller/SearchController.java
+++ src/main/java/kosta/mvc/controller/SearchController.java
@@ -1,6 +1,10 @@
 package kosta.mvc.controller;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -9,8 +13,11 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import kosta.mvc.dto.Post;
+import kosta.mvc.dto.Study;
+import kosta.mvc.paging.PageCnt;
 import kosta.mvc.service.SearchService;
 import kosta.mvc.service.SearchServicelmpl;
+import kosta.mvc.util.DbUtil;
 
 
 public class SearchController implements Controller{
@@ -85,5 +92,46 @@ public class SearchController implements Controller{
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	public ModelAndView selectTitleStudy(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String studyTitle = request.getParameter("studyTitle");;
+		Connection con=null;
+		PreparedStatement ps=null;
+		ResultSet rs=null;
+		
+		List<Study> studyList = new ArrayList<>();
+		String sql = "SELECT * FROM study WHERE study_title LIKE ?"; //where rnum <=? and rnum>=?
+		try {
+	
+			con = DbUtil.getConnection();
+			ps = con.prepareStatement(sql);
+			ps.setString(1, "%"+studyTitle+"%");
+			
+			rs = ps.executeQuery();
+			
+			
+			while(rs.next()) {
+				Study study = new Study(rs.getString(1), rs.getInt(2), rs.getString(3), rs.getString(4),
+						rs.getString(5), rs.getString(6), rs.getString(7));
+				
+				studyList.add(study);
+			}
+			
+			for(Study s : studyList) {
+				//System.out.println(s.getUserId());
+			}
+			
+		}finally {
+			DbUtil.dbClose(rs, ps, con);
+		}
+		
+		List<Study> list = studyList;
+		System.out.println(list);
+		
+		request.setAttribute("studyList", list);
+		
+
+		return new ModelAndView("study/list.jsp"); //이동 위치
 	}
 }
